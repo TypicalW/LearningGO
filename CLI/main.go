a0a543f9e14146a823d067acbb2c0658f2bdff5e
TypicalW <ashmit1665@gmail.com>
2026-09-09 10:14:03 +0530

    Added structs and functions

--- CLI/main.go
+++ CLI/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"time"
+)
+
+type Todo struct {
+	Title      string
+	Status     bool
+	StartTime  time.Time
+	EndTime    *time.Time
+	Completion uint8
+}
+
+type Todos []Todo
+
+func (todos *Todos) add(title string) {
+	todo := Todo{
+		Title:      title,
+		Status:     false,
+		StartTime:  time.Now(),
+		EndTime:    nil,
+		Completion: 0,
+	}
+
+	*todos = append(*todos, todo)
+}
