b8fb5246268f3045b64de6aa8a56c87195e7aa9e
TypicalW <ashmit1665@gmail.com>
2026-09-13 23:41:38 +0530

    Added more functions

--- CLI/main.go
+++ CLI/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
+
+	"github.com/aquasecurity/table"
 )
 
 // Created a data-type holding bluerprint for To-do objects
@@ -44,4 +48,51 @@ func (todos *Todos) delete(index int) error {
 	}
 	*todos = append(t[:index], t[index+1:]...)
 	return nil
+}
+
+func (todos *Todos) toggle(index int) error {
+	if err := todos.validateIndex(index); err != nil {
+		return err
+	}
+	t := *todos
+	todo := &t[index]
+
+	if !todo.Status {
+		completedTime := time.Now()
+		todo.EndTime = &completedTime
+	} else {
+		todo.EndTime = nil
+	}
+	todo.Status = !todo.Status
+	return nil
+
+}
+
+func (todos *Todos) edit(index int, title string) error {
+	if err := todos.validateIndex(index); err != nil {
+		return err
+	}
+	(*todos)[index].Title = title
+	return nil
+}
+
+func (todos *Todos) list() {
+	table := table.New(os.Stdout)
+	table.SetRowLines(true)
+	table.SetHeaders("#", "Task", "Status", "Started at", "Finished at")
+
+	for index, t := range *todos {
+		completed := "X"
+		completedAt := ""
+
+		if t.Status {
+			completed = "DONE"
+			if t.EndTime != nil {
+				completedAt = t.EndTime.Format(time.RFC1123)
+			}
+		}
+		table.AddRow(strconv.Itoa(index), t.Title, completed, t.StartTime.Format(time.RFC1123), completedAt)
+
+	}
+	table.Render()
 }
