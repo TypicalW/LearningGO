6ff01adb7f1a1783365ffe6cd78ff8c3502b4b05
TypicalW <ashmit1665@gmail.com>
2026-09-13 22:34:43 +0530

    Added few new functions

--- CLI/main.go
+++ CLI/main.go
@@ -1,27 +1,47 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"time"
 )
 
+// Created a data-type holding bluerprint for To-do objects
 type Todo struct {
-	Title      string
-	Status     bool
-	StartTime  time.Time
-	EndTime    *time.Time
-	Completion uint8
+	Title     string
+	Status    bool
+	StartTime time.Time
+	EndTime   *time.Time
 }
 
+// created a data-type with type being []Todo which is a slice
+// Todos is a datatype holding Todo structs
 type Todos []Todo
 
 func (todos *Todos) add(title string) {
 	todo := Todo{
-		Title:      title,
-		Status:     false,
-		StartTime:  time.Now(),
-		EndTime:    nil,
-		Completion: 0,
+		Title:     title,
+		Status:    false,
+		StartTime: time.Now(),
+		EndTime:   nil,
 	}
 
 	*todos = append(*todos, todo)
+}
+
+func (todos *Todos) validateIndex(index int) error {
+	if index < 0 || index >= len(*todos) {
+		err := errors.New("Invalid index bozo")
+		fmt.Println(err)
+	}
+	return nil
+}
+
+func (todos *Todos) delete(index int) error {
+	t := *todos
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+	*todos = append(t[:index], t[index+1:]...)
+	return nil
 }
